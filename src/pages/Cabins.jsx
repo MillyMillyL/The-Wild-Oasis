690832d12a162f77e446cd6f7a588ec1911ca87a
Milly <liuhongmein@gmail.com>
2023-09-27 10:04:39 -0400

    0927

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +16,11 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add New Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
