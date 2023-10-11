f75103009e685f32403145ab0274bea6a290002e
Milly <liuhongmein@gmail.com>
2023-10-11 09:11:28 -0400

    1011

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -6,11 +6,12 @@ import toast from "react-hot-toast";
 export function useLogin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isLoading: isLogingIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (data) => {
-      queryClient.setQueriesData(["user"], data);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], data.user);
+      navigate("/dashboard", { replace: true });
 
       toast.success(`${data.user.email} successfully logged in`);
     },
