33da4f57b8ca2978270dbdc82bd0c95a26aa5048
Milly <liuhongmein@gmail.com>
2023-10-19 12:56:15 -0400

    1019finish

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -87,6 +87,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleToggle(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -94,6 +95,7 @@ function Toggle({ id }) {
     });
     openId === "" || openId !== id ? open(id) : close();
   }
+
   return (
     <StyledToggle onClick={handleToggle}>
       <HiEllipsisVertical />
@@ -103,7 +105,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
   return createPortal(
